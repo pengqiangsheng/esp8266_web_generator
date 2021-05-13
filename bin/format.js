fce86098a5f16309a776f97414762994b9d2348a
pengqiangsheng <2945802136@qq.com>
2021-05-13 22:09:40 +0800

    自动化

--- bin/format.js
+++ bin/format.js
@@ -1,34 +1,35 @@
 const fs = require('fs')
-
-console.log('格式化html开始.')
+const debug = require('debug')('app:format')
+debug('格式化html开始.')
 
 const command = [...process.argv].pop()
 
-console.log('尝试读取命令行参数...')
+debug('尝试读取命令行参数...')
 
-if(command.includes('http://') || command.includes('https://')) console.log('命令行参数读取成功:', command)
-else console.log('命令行参数无法读取，将使用默认参数')
+if(command.includes('http://') || command.includes('https://')) debug('命令行参数读取成功:', command)
+else debug('命令行参数无法读取，将使用默认参数')
 
-const urlPrefix = command || 'http://img.pqs.guozhaoxi.top/mqtt-vue-'
+// const urlPrefix = command || 'http://img.pqs.guozhaoxi.top/mqtt_vue_static_'
+const urlPrefix = 'http://img.pqs.guozhaoxi.top/mqtt_vue_static_'
 
-console.log('准备打开源文件...')
+debug('准备打开源文件...')
 
 fs.readFile('./dist/index.html', function (error, data) {
   if (error) {
-    console.log('文件打开错误...')
+    debug('文件打开错误...')
   } else {
-    console.log('源文件已打开正在转换格式...')
+    debug('源文件已打开正在转换格式...')
     const buf = data.toString()
       .replace(/static\/js\//g, urlPrefix)
       .replace(/static\/css\//g, urlPrefix)
       .replace(/\"/g, '\'')
-    console.log('源文件转化完毕，正在写入...')
+    debug('源文件转化完毕，正在写入...')
     const fileName = './dist/index-' + randomString(8) + '.html'
     fs.writeFile(fileName, buf, function (error) {
       if (error) {
-        console.log('写入失败...')
+        debug('写入失败...')
       } else {
-        console.log('写入成功，文件生成在: ', fileName)
+        debug('写入成功，文件生成在: ', fileName)
       }
     })
   }
--- bin/qiniu.js
+++ bin/qiniu.js
@@ -1,7 +1,11 @@
 const qiniu = require('qiniu')
+const fs = require('fs')
+const { join } = require('path')
+const debug = require('debug')('app:qiniu')
 const accessKey = 'CidmCcGziRO7UOENyCqmG-FQ9E3NJfEvW8G_evac'
 const secretKey = 'qKP4BfkhPOO1E28cXJXNRh-G48V40LYJEXSfN5VJ'
 const mac = new qiniu.auth.digest.Mac(accessKey, secretKey)
+const urlPrefix = 'mqtt_vue_static_'
 
 const options = {
   scope: 'pengqiangsheng',
@@ -10,23 +14,46 @@ const options = {
 const putPolicy = new qiniu.rs.PutPolicy(options)
 const uploadToken = putPolicy.uploadToken(mac)
 
-console.log(uploadToken)
-var config = new qiniu.conf.Config();
+debug(uploadToken)
 
-var localFile = "./bin/test.md";
-var formUploader = new qiniu.form_up.FormUploader(config);
-var putExtra = new qiniu.form_up.PutExtra();
-var key='test.txt';
-// 文件上传
-formUploader.putFile(uploadToken, key, localFile, putExtra, function(respErr,
-  respBody, respInfo) {
-  if (respErr) {
-    throw respErr;
-  }
-  if (respInfo.statusCode == 200) {
-    console.log(respBody);
-  } else {
-    console.log(respInfo.statusCode);
-    console.log(respBody);
-  }
+const config = new qiniu.conf.Config();
+
+const formUploader = new qiniu.form_up.FormUploader(config);
+
+const putExtraJS = new qiniu.form_up.PutExtra({
+  mimeType: 'application/javascript'
 });
+const putExtraCSS = new qiniu.form_up.PutExtra({
+  mimeType: 'text/css'
+});
+
+const upload = (localFile, key, putExtra) => {
+  debug('上传: ', localFile, ' 到七牛云...')
+  // 文件上传
+  formUploader.putFile(uploadToken, key, localFile, putExtra, function(respErr,
+    respBody, respInfo) {
+    if (respErr) {
+      throw respErr;
+    }
+    if (respInfo.statusCode == 200) {
+      debug(respBody);
+    } else {
+      debug(respInfo.statusCode);
+      debug('已上传到：'+ respBody.bucket + respBody.key);
+    }
+  });
+}
+
+fs.readdirSync(join(__dirname, '../dist/static/js')).reverse().forEach(file => {
+  if(file.endsWith('.gz')) return
+  let fn = join(__dirname, '../dist/static/js', file)
+  upload(fn, urlPrefix + file, putExtraJS)
+})
+
+fs.readdirSync(join(__dirname, '../dist/static/css')).reverse().forEach(file => {
+  if(file.endsWith('.gz')) return
+  let fn = join(__dirname, '../dist/static/css', file)
+
+  upload(fn, urlPrefix + file, putExtraCSS)
+})
+
